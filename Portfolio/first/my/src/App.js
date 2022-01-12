f169e82f1d1fbc1e4490cc093217b19becf083c9
aka-ryu <uhas2003@naver.com>
2022-01-12 19:42:54 +0900

    2021/01/12

--- Portfolio/first/my/src/App.js
+++ Portfolio/first/my/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Nav from './component/nav.js';
+import Main from './component/main.js';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Main></Main>
     </div>
   );
 }
