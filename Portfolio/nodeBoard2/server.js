33bd210b4935ff6ea2d0a6b225837dd405880155
aka-ryu <uhas2003@naver.com>
2022-02-11 00:59:43 +0900

    2022/02/11

--- Portfolio/nodeBoard2/server.js
+++ Portfolio/nodeBoard2/server.js
@@ -9,7 +9,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
+app.use(session({secret : 'key', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
 
 var db;
 
@@ -40,12 +46,14 @@ app.get('/list/:id', async function(req, resp){
 
     const page = parseInt(req.params.id) || 1;
     const pagenation = (Math.floor(((page*10)-10)/100) );
-    const skipSize = (page-1) * 3;
-    const limitSize = 3;
-    const maxPage = Math.ceil(await db.collection('board').count() / 3);
+    const skipSize = (page-1) * 10;
+    const limitSize = 10;
+    const maxPage = Math.ceil(await db.collection('board').count() / 10);
     const startPage = maxPage - 9;
     const pageSize = 10;
+    const user = req.user || "비회원";
    
+
 
     db.collection('board').find().sort({_id : -1}).skip(skipSize).limit(limitSize).toArray((function(err, result){
     resp.render('list.ejs', 
@@ -55,13 +63,16 @@ app.get('/list/:id', async function(req, resp){
                     maxPage : maxPage,
                     pageSize : pageSize,
                     startPage : startPage,
-                    pagenation : pagenation
+                    pagenation : pagenation,
+                    user : user
                 });
     }));
 });
 
-app.get('/register', function(req, resp){
-    resp.render('register.ejs');
+app.get('/register',loginCheck, function(req, resp){
+    const user = req.user;
+    console.log(user);
+    resp.render('register.ejs', {user : user});
 });
 
 app.post('/add', function(req, resp){
@@ -85,11 +96,12 @@ app.post('/add', function(req, resp){
 
 
 app.get('/detail/:id', function(req, resp){
-    
+    const user = req.user || "비회원";
+
     db.collection('board').findOne({ _id : parseInt(req.params.id) }, function(err, result){
         if(err) return console.log(err);
 
-        resp.render('detail.ejs',{ data : result});
+        resp.render('detail.ejs',{ data : result, user: user});
     });
 
 });
@@ -127,4 +139,80 @@ app.get('/search', function(req, resp){
         console.log(result);
         resp.render('search.ejs', { board : result })
     });
+});
+
+app.get('/login', function(req, resp){
+    resp.render('login.ejs');
+})
+
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(req, resp){
+    resp.redirect('/');
+})
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (inputid, inputpw, done) {
+    
+    db.collection('user').findOne({ id: inputid }, function (err, result) {
+        if (err) return done(err)
+
+        if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
+    
+        if (inputpw == result.pw) {
+        return done(null, result)
+        } else {
+        return done(null, false, { message: '비번틀렸어요' })
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(userid, done){
+    db.collection('user').findOne({id: userid}, function(err, result){
+
+        done(null, result)
+    })
+});
+
+app.get('/fail', function(req, resp){
+    resp.send("<script type='text/javascript'>  window.location='/login'; alert('아이디,비밀번호를 확인하세요.'); </script>");
+})
+
+function loginCheck(req, resp, next) {
+    if(req.user){
+        next()
+    } else {
+        resp.send("<script type='text/javascript'> alert('글 작성은 회원만 가능합니다.'); window.location='/'; </script>");
+    }
+};
+
+
+app.get('/logout', function(req, resp){
+    req.session.destroy(function(err){
+        if(err) console.log(err);
+
+        resp.redirect('/');
+    })
+})
+
+app.get('/join', function(req, resp){
+    resp.render('join.ejs')
+})
+
+app.post('/join', function(req,resp){
+    db.collection('user').findOne({id: req.body.id}, function(err, result){
+        if(result == null) {
+            db.collection('user').insertOne({id: req.body.id, pw: req.body.pw});
+            resp.send("<script type='text/javascript'> alert('회원가입이 완료되었습니다.'); window.location='/'; </script>");
+        } else if(result != null) {
+            resp.send("<script type='text/javascript'> alert('중복된 아이디가 존재합니다.'); window.location='/join'; </script>");
+        }
+    });
+    
 });
